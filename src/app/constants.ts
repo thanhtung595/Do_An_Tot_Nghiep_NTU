e7086f52882f2291f6c7968088a4f0001746bc4d
thanhtung595 <nguyenthanhtung.06112003@gmail.com>
2025-06-10 10:24:20 +0700

    ab

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,2 +1,4 @@
+import { environment } from '../environments/environment';
+
 export const API_BASE_URL_OLD = 'http://localhost:8000/';
-export const API_BASE_URL = 'http://54.151.174.65:8080/';
+export const API_BASE_URL = environment.API_BASE_URL;
