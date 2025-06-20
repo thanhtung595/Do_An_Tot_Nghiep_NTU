ef4f871b931d90c40f4b64fa533321e7182a13e1
thanhtung595 <nguyenthanhtung.06112003@gmail.com>
2025-06-20 14:59:45 +0700

    final

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 // export const API_BASE_URL = 'https://doantotnghiep.pro/';
 export const API_BASE_URL_OLD = 'https://do-an-tot-nghiep-ntu-api-gemini.onrender.com/';
-export const API_BASE_URL = 'http://localhost:8080/';
+export const API_BASE_URL = 'https://doantotnghiep.pro/';
